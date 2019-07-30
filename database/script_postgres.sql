d33468c701de8ed5f73c6e0456cc3e701ac7e0a5
dmyr0613 <dmyr0613@gmail.com>
2019-07-30 20:50:43 +0900

    1

---
+++ database/script_postgres.sql
@@ -0,0 +1,8 @@
+create table calling (
+  device_name varchar(50) not null,
+  msg_no int,
+  comment varchar(100)
+);
+
+insert into calling values('All', 0, '全職員へCall');
+insert into calling values('dmyr-iPhone6s', 0, '太田');
