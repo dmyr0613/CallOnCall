80afd394a6c179f1653ce6e95f4602dc0c37b3b9
dmyr0613 <dmyr0613@gmail.com>
2019-08-01 19:19:55 +0900

    1

--- database/script_postgres.sql
+++ database/script_postgres.sql
@@ -9,5 +9,9 @@ insert into calling values('dmyr-iPhone6s', 0, '太田');
 
 update calling set msg_no = 1 where device_name = 'dmyr-iPhone6s';
 
---webAPI
+--msg_flgの取得
 http://call-on-call.herokuapp.com/calling-get.php?device_name=dmyr-iPhone6s
+--msg_flgのクリア
+http://call-on-call.herokuapp.com/calling-clear.php?device_name=dmyr-iPhone6s
+--デバイス名の存在チェック
+http://call-on-call.herokuapp.com/calling-check.php?device_name=dmyr-iPhone6s
