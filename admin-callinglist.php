46e1920a345dd96032a5979d80ec49b1816c4014
dmyr0613 <dmyr0613@gmail.com>
2019-08-02 18:21:24 +0900

    1

---
+++ admin-callinglist.php
@@ -0,0 +1,39 @@
+<?php session_start(); ?>
+<?php require 'admin-header.php'; ?>
+
+<!-- Main -->
+	<div id="main">
+		<div class="inner">
+
+				<!-- Header -->
+				<?php require 'admin-header-sub.php'; ?>
+
+				<!-- reserveMain -->
+				<section id="reserveMain">
+					<?php
+
+						// デバイス情報を取得する。
+						$sql=$pdo->prepare('select * from calling order by device_name');
+						$sql->execute();
+
+						echo '<form action="admin-callinglist.php" method="post">';				//送信用のpost
+						echo '<table>';
+						echo '<th>デバイス名</th><th>メッセージNo</th><th>備考</th>';
+						foreach ($sql as $row) {
+							echo '<tr>';
+							echo '<td>', $row['device_name'], '</td>';
+							echo '<td>', $row['msg_no'], '</td>';
+							echo '<td>', $row['name'], '</td>';
+						}
+						echo '</table>';
+						echo '<input type="submit" class="button primary small" value="Register">';
+						echo '</form>';
+
+					?>
+				</section>
+
+			</div>
+		</div>
+
+<?php require 'admin-menu.php'; ?>
+<?php require 'footer.php'; ?>
---
+++ admin-menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="admin-callinglist.php">呼び出し一覧 CallingList</a></li>
+										<li><a href="admin-beacon.php">メッセージマスタ Message Master</a></li>
+									</ul>
+								</nav>
+
+						</div>
+					</div>
+
+</html>
