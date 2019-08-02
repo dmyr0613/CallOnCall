1b1b5171d33bb89f9d025084504ae1ab6f4d6a38
dmyr0613 <dmyr0613@gmail.com>
2019-08-02 20:17:52 +0900

    1

--- admin-callingsend.php
+++ admin-callingsend.php
@@ -17,34 +17,29 @@
 						foreach ($obj as $key => $val){
 							error_log($key);
 
-							// echo($key);
-							// echo($_REQUEST['msg_no']);
-							// echo($key['msg_no']);
-							// echo($val['msg_no']);
-
 							if (substr_count($key, 'msg_no_') == 1) {
 								//文字列にline_idが含まれる場合
 								$device_name = substr($key, 7);	//デバイス名を抜き取る。
 								error_log($device_name);
+								$msg_no = $_REQUEST['msg_no_' . $device_name];
+								error_log($msg_no);
 
-								echo($device_name);
-								echo($_REQUEST['msg_no_' . $device_name]);
+								if ($msg_no > 1) {
+									// $options = '{"contacts": [{ "phone_number": "09076114485" }], "text_message": "' . $message2 . '" }';
+									// $options = '{"contacts": [{ "phone_number": "' . $key . '" }], "text_message": "' . $message2 . '" }';
+									$url = "http://call-on-call.herokuapp.com/calling-set.php?device_name=" . $device_name . "&msg_no=" . $msg_no;
+									$ch = curl_init();
+									curl_setopt($ch, CURLOPT_URL, $url);
+									// curl_setopt($ch, CURLOPT_HTTPHEADER, array('token: ', 'Content-Type: application/json'));
+									// curl_setopt($ch, CURLOPT_POST, 1);
+									// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($options));
+									// curl_setopt($ch, CURLOPT_POSTFIELDS, $options);
+									// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-
-								// $options = '{"contacts": [{ "phone_number": "09076114485" }], "text_message": "' . $message2 . '" }';
-								// $options = '{"contacts": [{ "phone_number": "' . $key . '" }], "text_message": "' . $message2 . '" }';
-								$url = "http://call-on-call.herokuapp.com/calling-set.php?device_name=dmyr-iPhone6s&msg_no=" . $_REQUEST['msg_no'];
-								$ch = curl_init();
-								curl_setopt($ch, CURLOPT_URL, $url);
-								// curl_setopt($ch, CURLOPT_HTTPHEADER, array('token: ', 'Content-Type: application/json'));
-								// curl_setopt($ch, CURLOPT_POST, 1);
-								// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($options));
-								// curl_setopt($ch, CURLOPT_POSTFIELDS, $options);
-								// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-								// $response = curl_exec($ch);
-								curl_close($ch);
-								error_log(print_r($response, true));
+									$response = curl_exec($ch);
+									curl_close($ch);
+									error_log(print_r($response, true));
+								}
 							}
 						}
 
