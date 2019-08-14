c3ac0dd2d8900089e427948e55e6d225e4207a0e
dmyr0613 <dmyr0613@gmail.com>
2019-08-14 15:51:10 +0900

    1

--- admin-header.php
+++ admin-header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title>CallOnCall | 管理者画面</title>
-<link rel="shortcut icon" type="image" href="images/admin-favicon.ico">
+<link rel="shortcut icon" type="image" href="images/favicon.ico">
 <meta http-equiv="Content-Type" content="text/html; charset=shift_jis">
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
