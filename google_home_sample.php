38e97ae5d736845c897ae130c87185e5dfbc4d9d
dmyr0613 <dmyr0613@gmail.com>
2019-09-16 10:34:50 +0900

    1

--- google_home_sample.php
+++ google_home_sample.php
@@ -9,11 +9,42 @@ if($update["queryResult"]["action"] == "getDate"){
     $month      = intval($update["queryResult"]["parameters"]["month"]);
     $year       = intval($update["queryResult"]["parameters"]["year"]);
     $speech_txt = $year . "年" . $month . "月って言いましたか？";
+
+    //meg_noの指定がある場合は、webAPIにて、メッセージNoを更新する。
+    $url = "http://call-on-call.herokuapp.com/calling-set.php?device_name=dmyr-iPhone6s&msg_no=" . $month;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+    error_log(print_r($response, true));
+
   } else {
     $speech_txt = "分かりませんでした。もう一度、年月を言い直してください。切断する場合は終了と言ってください。";
   }
   processMessage($update,$speech_txt);
 }
+
+if($update["queryResult"]["action"] == "getMsgNo"){
+  if (isset($update["queryResult"]["parameters"]["msgNo"])) {
+    $msg_no      = intval($update["queryResult"]["parameters"]["msgNo"]);
+    $speech_txt = $msg_no . "番を送信しました。";
+
+    //meg_noの指定がある場合は、webAPIにて、メッセージNoを更新する。
+    $url = "http://call-on-call.herokuapp.com/calling-set.php?device_name=dmyr-iPhone6s&msg_no=" . $msg_no;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+    error_log(print_r($response, true));
+
+  } else {
+    $speech_txt = "分かりませんでした。もう一度、番号を言い直してください。";
+  }
+  processMessage($update,$speech_txt);
+}
+
 // default Welcome Intent用（blog参照）
 if($update["queryResult"]["action"] == "getWelcome"){
   $speech_txt   = "必要に応じて番号を発言してください。";
