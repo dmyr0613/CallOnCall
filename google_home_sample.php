526186f970683b991d497b6268317164ee7fa6a6
dmyr0613 <dmyr0613@gmail.com>
2019-09-16 10:03:11 +0900

    1

--- google_home_sample.php
+++ google_home_sample.php
@@ -4,10 +4,10 @@ header('Content-type: application/json');
 $update_response = file_get_contents("php://input");
 $update          = json_decode($update_response, true);
 // ここのgetDateがIntentのAction and parametersと対応している
-if($update["result"]["action"] == "getDate"){
-  if (isset($update["result"]["parameters"]["month"])) {
-    $month      = intval($update["result"]["parameters"]["month"]);
-    $year       = intval($update["result"]["parameters"]["year"]);
+if($update["queryResult"]["action"] == "getDate"){
+  if (isset($update["queryResult"]["parameters"]["month"])) {
+    $month      = intval($update["queryResult"]["parameters"]["month"]);
+    $year       = intval($update["queryResult"]["parameters"]["year"]);
     $speech_txt = $year . "年" . $month . "月って言いましたか？";
   } else {
     $speech_txt = "分かりませんでした。もう一度、年月を言い直してください。切断する場合は終了と言ってください。";
@@ -15,16 +15,14 @@ if($update["result"]["action"] == "getDate"){
   processMessage($update,$speech_txt);
 }
 // default Welcome Intent用（blog参照）
-if($update["result"]["action"] == "getWelcome"){
+if($update["queryResult"]["action"] == "getWelcome"){
   $speech_txt   = "いらっしゃいませ";
   processMessage($update,$speech_txt);
 }
 // 返信用Json
 function processMessage($update,$speech_txt) {
     echo json_encode(array(
-        "source" => $update["result"]["source"],
-        "speech" => $speech_txt,
-        "displayText" => $speech_txt,
+        "fulfillmentText" => $speech_txt,
         "contextOut" => array()
     ));
 }
