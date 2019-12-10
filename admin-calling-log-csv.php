a8de2cf30f92248c02441cba0634fc324dd0d4d1
dmyr0613 <dmyr0613@gmail.com>
2019-12-10 21:25:23 +0900

    1

--- admin-calling-log-csv.php
+++ admin-calling-log-csv.php
@@ -1,33 +1,44 @@
-<?php session_start(); ?>
-<?php require 'admin-header.php'; ?>
+<?php
 
-<!-- Main -->
-	<div id="main">
-		<div class="inner">
+// 出力情報の設定
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=GRAYCODE.csv");
+header("Content-Transfer-Encoding: binary");
 
-				<!-- Header -->
-				<?php require 'admin-header-sub.php'; ?>
+// 変数の初期化
+$member = array();
+$csv = null;
 
-				<!-- reserveMain -->
-				<section id="reserveMain">
-					<?php
+// 出力したいデータのサンプル
+$member = array(
+	array(
+		'id' => 1,
+		'name' => '山田太郎',
+		'furigana' => 'やまだたろう',
+		'email' => 'taroyamada@sample.com'
+	),
+	array(
+		'id' => 3,
+		'name' => '加藤明美',
+		'furigana' => 'かとうあけみ',
+		'email' => 'akemikato@sample.com'
+	),
+	array(
+		'id' => 5,
+		'name' => '佐藤健夫',
+		'furigana' => 'さとうたけお',
+		'email' => 'takeosato@sample.com'
+	)
+);
 
-					// ダウンロードするサーバのファイルパス
-					$filepath = 'sample.csv';
+// 1行目のラベルを作成
+$csv = '"ID","氏名","ふりがな","メールアドレス"' . "\n";
 
-					// HTTPヘッダを設定
-					header('Content-Type: application/octet-stream');
-					header('Content-Length: '.filesize($filepath));
-					header('Content-Disposition: attachment; filename=download.csv');
+// 出力データ生成
+foreach( $member as $value ) {
+	$csv .= '"' . $value['id'] . '","' . $value['name'] . '", "' . $value['furigana'] . '","' . $value['email'] . '"' . "\n";
+}
 
-					// ファイル出力
-					readfile($filepath);
-
-					?>
-				</section>
-
-			</div>
-		</div>
-
-<?php require 'admin-menu.php'; ?>
-<?php require 'footer.php'; ?>
+// CSVファイル出力
+echo $csv;
+return;
