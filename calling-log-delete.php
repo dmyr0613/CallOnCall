a1ed012e31fd73061182317543db0cd2ac788eb6
dmyr0613 <dmyr0613@gmail.com>
2019-12-13 16:46:01 +0900

    1

---
+++ calling-log-delete.php
@@ -0,0 +1,46 @@
+<?php
+
+try{
+
+	if (!empty($_GET)) {
+
+    //Heroku PostgresSQL
+    // $dsn = 'pgsql:dbname=d13p6kmhdcirvm host=ec2-174-129-208-118.compute-1.amazonaws.com port=5432';
+    // $user = 'gkijtxlavebgol';
+    // $password = 'ecff643bfa3612a94627c9d668f867a06ce4b86e4a69f8a42d981af26c50a505';
+
+	  // Kaimeido Heroku PostgresSQL
+	  $dsn = 'pgsql:dbname=d8qp7bgte2p1ue host=ec2-174-129-254-249.compute-1.amazonaws.com port=5432';
+	  $user = 'crridrugfblfyo';
+	  $password = '345e1a510b468c26ac2149fa0d42d2b947fe8a762a34c89c027a906539cd0895';
+    $pdo = new PDO($dsn, $user, $password);
+
+    if ($pdo == null){
+      error_log("接続に失敗しました。");
+    }else{
+      error_log("接続に成功しました。");
+    }
+
+    //デバイス名を取得
+    $device_name = $_GET['device_name'];
+
+    //Calling_logから100件より前のデータを削除する
+    $sqlText   = 'delete from calling_log';
+    $sqlText  .= ' order by insert_dateteime desc limit 10';
+    error_log(print_r($sqlText, true));
+
+    $sql=$pdo->prepare($sqlText);
+    $sql->execute();
+		$count = $sql->rowCount();
+		error_log("過去データ削除：" . $count . "件");
+
+    //DB接続情報をクリア
+    $pdo = null;
+  }
+
+}catch (PDOException $e){
+  print('Error:'.$e->getMessage());
+  die();
+}
+
+?>
